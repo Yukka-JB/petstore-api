f1846f514e39626c68849798f39b557b869a19be
jbobrowski <jbobrowski@loreto.ac.uk>
2025-09-30 19:41:34 +0100

    updated service class with methods

---
+++ app/Services/PetstoreApiService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\RequestException;
+
+class PetstoreApiService
+{
+    protected string $baseUrl = 'https://petstore.swagger.io/v2/pet';
+
+    public function getPet(int $petId): array {
+
+        $response = Http::get("{$this->baseUrl}/{$petId}");
+
+        return $this->handleResponse($response);
+    }
+
+    public function addPet(array $petData): array {
+
+        $response = Http::post($this->baseUrl, $petData);
+
+        return $this->handleResponse($response);
+    }
+
+    public function deletePet(int $petId): bool {
+        $response = Http::delete("{$this->baseUrl}/{$petId}");
+
+        if ($response->successful()) {
+            return true;
+        }
+
+        throw new RequestException($response);
+
+    }
+
+    protected function handleResponse($response): array {
+        
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        throw new RequestException($response);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PetController;
+
+Route::get('/pets/create', [PetController::class, 'create'])->name('pets.create');
+Route::post('/pets', [PetController::class, 'store'])->name('pets.store');
+Route::get('/pets/{id}', [PetController::class, 'show'])->name('pets.show');
+Route::get('/pets/{id}/edit', [PetController::class, 'edit'])->name('pets.edit');
+Route::put('/pets/{id}', [PetController::class, 'update'])->name('pets.update');
+Route::delete('/pets/{id}', [PetController::class, 'destroy'])->name('pets.destroy');
